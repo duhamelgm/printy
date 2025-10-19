1734288845e036608b82ec902ba9958a8dce8ae1
Duhamel Guerrero <duhamelgm@gmail.com>
2025-10-19 11:42:37 -0400

    Change to get

--- internal/server/server.go
+++ internal/server/server.go
@@ -68,7 +68,7 @@ func (s *Server) Start() error {
 
 // handlePrint handles print requests
 func (s *Server) handlePrint(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
+	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
